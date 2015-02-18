38e9ea538e0b08fc9256edebdde9b0021d0e6b39
Thilanka N. Liyanaarachchi <catchthila@gmail.com>
2015-02-18 22:09:32 +0530

    Initial commit

---
+++ js/validation.js
@@ -0,0 +1,262 @@
+//Defining global namespace JQBSTVALIDATOR if not exists
+var JQBSTVALIDATOR = JQBSTVALIDATOR || {};
+JQBSTVALIDATOR.lib = {};
+//Creating an alias for quick access
+var $V = JQBSTVALIDATOR.lib;
+$V.settings = {
+    dyn:false,
+    animate:false,
+    iconFeedback:false,
+    tes:"TEST"
+};
+$V.load = function(custom){$.extend(true,$V.settings,custom);};
+    //Handles validation
+    $(function(){
+        /**
+            Following section will create OO clases
+        */
+        
+        $V.Validator = function(rules){};
+        $V.Validator.prototype.name = "Default Name";
+        $V.Validator.prototype.rules = [];
+        $V.Validator.prototype.validate = function(fieldValue, label){
+            //Generate Rules Array
+            this.rules = this.name.split(",");
+            var errorMessage = "";
+            for(i  = 0; i < this.rules.length; i++){
+                if(this.rules[i]==="not empty" && fieldValue===""){
+                    errorMessage = label+" can not be empty";
+                    //return errorMessage;
+                }else if(this.rules[i]==="number"){
+                    errorMessage = this.numberValidate(fieldValue, label);
+                    //return errorMessage;
+                }else if(this.rules[i].match(/number\(\d+-\d+\)/)){
+                    errorMessage = this.numberRangeValidate(this.rules[i], fieldValue, label);
+                    //return errorMessage;
+                }else if(this.rules[i] === "email"){
+                    errorMessage = this.emailValidate(fieldValue);
+                }else if(this.rules[i].match(/length\(\d+\)/)){
+                    errorMessage = this.lengthValidate(this.rules[i], fieldValue, label);
+                    //return errorMessage;
+                }else if(this.rules[i].match(/minLength\(\d+\)/)){
+                    errorMessage = this.minLengthValidate(this.rules[i], fieldValue, label);
+                    //return errorMessage;
+                }else if(this.rules[i].match(/maxLength\(\d+\)/)){
+                    errorMessage = this.maxLengthValidate(this.rules[i], fieldValue, label);
+                    //return errorMessage;
+                }else if(this.rules[i].match(/custom\(\w+\)/)){
+                    errorMessage = this.customValidate(this.rules[i], fieldValue);
+                    //return errorMessage;
+                }
+                if(errorMessage && errorMessage !== ""){
+                    return errorMessage;
+                }
+            }
+        };
+        /**
+            Returns nothing (undefined) if text is empty string or number, 
+            else an error string
+        */
+        $V.Validator.prototype.numberValidate = function(text, label){
+            text = (text === "") ? "1" : text; 
+            if(isNaN(parseInt(text))){
+                return "Enter a valid number for "+label;
+            }
+        };
+        /**
+            Returns nothing (undefined) if text is empty string.
+            If text is an invalid number, error string will be returned.
+            If number is within range, return nothing, else will return
+            an error string.
+        */
+        $V.Validator.prototype.numberRangeValidate = function(range,text,label){
+            var res = range.replace(/^number\(/,"").match(/\d+-\d+/)[0];
+            var min = res.split("-")[0];
+            var max = res.split("-")[1];
+            //Put a random number to text if it is left empty, so no error msg will be created
+            //text = (text === "") ? "1" : text; 
+            if(text !== ""){
+                //If text is 123abc, NUMBER NOT IN RANGE error is displayed! Fix it!
+                if(isNaN(parseInt(text))){
+                    return "Enter a valid number for "+label;
+                }else if(parseInt(text)<min||parseInt(text)>max){
+                    return "Enter a valid number between "+min+" and "+max+" for the "+label;
+                }
+            }
+        };
+        /**
+            Returns nothing (undefined) if text is empty string.
+            If text is an invalid email, an error string will be returned.
+        */
+        $V.Validator.prototype.emailValidate = function(text){
+            if(text !== "" && !(/\w+@\w+\.\w+/).test(text)){
+                return "Enter a valid email address. Eg: yourname@google.com";
+            }
+        };
+        /**
+            Returns nothing (undefined) if text is empty string.
+            If text length is not exactly equal to given value (eg: length(3))
+            an error string will be returned.
+        */
+        $V.Validator.prototype.lengthValidate = function(rule,text,label){
+            var length = rule.replace(/length\(/,"").match(/\d+/)[0];
+            if(text !== "" && text.length != length){
+                return "Value should be exactly "+length+" characters long for the "+label;
+            }
+        };
+        /**
+            Returns nothing (undefined) if text is empty string.
+            If text length is less than the given min value (eg: minLength(3))
+            an error string will be returned.
+        */
+        $V.Validator.prototype.minLengthValidate = function(rule,text,label){
+            var minLength = rule.replace(/minLength\(/,"").match(/\d+/)[0];
+            if(text !== "" && text.length < minLength){
+                return "Value should be at least "+minLength+" characters long for the "+label;
+            }
+        };
+        /**
+            Returns nothing (undefined) if text is empty string.
+            If text length is greater than the max char limit(eg: maxLength(3))
+            an error string will be returned.
+        */
+        $V.Validator.prototype.maxLengthValidate = function(rule,text,label){
+            var maxLength = rule.replace(/maxLength\(/,"").match(/\d+/)[0];
+            if(text !== "" && text.length > maxLength){
+                return "Value should not exceed "+maxLength+" characters for the "+label;
+            }
+        };
+        /**
+            Returns nothing (undefined) if text is empty string or returns user's 
+            custom function's return message (if there were any)
+        */
+        $V.Validator.prototype.customValidate = function(rule,text){
+            var methodName = rule.replace(/custom\(/,"").replace(")","").match(/\w+/)[0];
+            if(text !== ""){
+                return window[methodName](text);
+            }
+        };
+        //Object.preventExtensions($V.Validator);
+        
+        $("#bt").click(function (){
+            $(".error-field-text").css('visibility', 'visible')
+            $(".error-field-text").fadeTo(250,0);
+        });
+        
+        $("#bt2").click(function (){
+            $(".error-field-text").css('visibility', 'visible')
+            $(".error-field-text").fadeTo(250,1);
+        });
+        /**
+            If the user set $V.load({dyn:true}), this will enable key up 
+            listener validation.
+        */
+        function setupDynamic(){
+            $(".form-horizontal .form-group").find("input[type=text].form-control")
+            .keyup(function(event){
+                //Following code segment is repeating!
+                //********************************************************
+                var isValidate = $(this).data("validate");
+                    if(isValidate){
+                        console.log("VALIDATING!");
+                        //Get all validation rules
+                        var validationRules = isValidate;       
+                        var rule1 = new $V.Validator();
+                        rule1.name = validationRules;
+                        var fieldValue = $(this).val();
+                        //console.log();
+                        var labelText  = $($("label[for='"+this.id+"']")[0]).text();
+                        //$("label[for='"+$(this)[0].id+"']").text()
+                        var validationMessage = rule1.validate(fieldValue, $.trim(labelText).replace(":",""));
+                        if(validationMessage){//Error exists
+                            showFieldErrors($(this),validationMessage);
+                            isFormValid = false;
+                        }else{//Data submitted is valid
+                            clearFieldErrors($(this));
+                        }
+                    }
+                //********************************************************
+            });
+        }
+        
+        function init(){
+            
+            if($V.settings.dyn){
+                setupDynamic();
+            }
+            //Get validation forms, setup onsubmit
+            $("form").each(function(){
+                if($(this).data("validate")){
+                    $(this).submit(function(event){return validate($(this));});
+                }
+            });
+            /**
+                The following code will inject error field elements. This will inject
+                to other non validating groups as well!!
+            */
+            $(".form-horizontal .form-group").find("input[type=text].form-control")
+            .after("<span class='error-field-text alert-danger'>ERROR TEXT</span>").next()
+            .addClass("col-sm-12").css("visibility","hidden").css("textAlign","right");
+            /**
+                Add icon spans if user enables. Appended next to (after) error spans.
+                This will also add has-feedback class to the form-groups
+            */
+            if($V.settings.iconFeedback){
+                $(".form-horizontal .form-group").find(".error-field-text")
+                .after("<span aria-hidden='true'></span>").next()
+            .addClass("form-control-feedback").addClass("glyphicon").closest(".form-group")
+            .addClass("has-feedback");
+            //.css("visibility","hidden");
+            }
+            function validate(form){
+                /**
+                    The following code will select all validation required elements
+                */
+                var isFormValid = true;
+                $(form).find(".form-group input[type=text].form-control")
+                .each(function(){
+                    var isValidate = $(this).data("validate");
+                    if(isValidate){
+                        console.log("VALIDATING!");
+                        //Get all validation rules
+                        var validationRules = isValidate;       
+                        var rule1 = new $V.Validator();
+                        rule1.name = validationRules;
+                        var fieldValue = $(this).val();
+                        //console.log();
+                        var labelText  = $($("label[for='"+this.id+"']")[0]).text();
+                        //$("label[for='"+$(this)[0].id+"']").text()
+                        var validationMessage = rule1.validate(fieldValue, $.trim(labelText).replace(":",""));
+                        if(validationMessage){//Error exists
+                            showFieldErrors($(this),validationMessage);
+                            isFormValid = false;
+                        }else{//Data submitted is valid
+                            clearFieldErrors($(this));
+                        }
+                    }
+                });
+                return isFormValid;
+            }
+        }
+        
+        function showFieldErrors(field, errorMessage){ 
+            $(field).closest(".form-group").addClass("has-error");
+            $(field).closest(".form-group").removeClass("has-success");
+            $(field).siblings(".form-control-feedback").addClass("glyphicon-remove");
+            $(field).siblings(".form-control-feedback").removeClass("glyphicon-ok");
+            //$(field).siblings(".error-field-text").fadeTo(2,0.1);
+            $(field).siblings(".error-field-text").css("visibility","visible").text(errorMessage);
+            //$(field).siblings(".error-field-text").fadeTo(325,1);
+        }
+        
+        function clearFieldErrors(field){
+            $(field).closest(".form-group").removeClass("has-error");
+            $(field).closest(".form-group").addClass("has-success");
+            $(field).siblings(".form-control-feedback").addClass("glyphicon-ok");
+            $(field).siblings(".form-control-feedback").removeClass("glyphicon-remove");
+            $(field).siblings(".error-field-text").css("visibility","hidden");
+            //$(field).siblings(".error-field-text").fadeTo(250,0);
+        }
+        
+        init();
+    });
